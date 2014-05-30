e6e6999590339089348d9a6df206897fbd986eeb
Kaleb Lape <pingortle@gmail.com>
2014-05-30 15:26:12 -0500

    Coreslab-ify it.

--- app/scripts/routes/application_route.js
+++ app/scripts/routes/application_route.js
@@ -2,6 +2,6 @@ CoreslabMockup.ApplicationRoute = Ember.Route.extend({
     // admittedly, this should be in IndexRoute and not in the
     // top level ApplicationRoute; we're in transition... :-)
     model: function () {
-        return ['red', 'yellow', 'blue'];
+        return ['Production', 'Admin', 'Scheduling', 'QC', 'Shipping/Erection', 'D&E', 'Finance'];
     }
 });
