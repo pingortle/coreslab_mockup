ecc7c35acff263108678dfa76dc152d1ab9c6136
Kaleb Lape <pingortle@gmail.com>
2014-06-06 11:52:51 -0500

    Fix warning about endefined mixin.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,7 @@
 var CoreslabMockup = window.CoreslabMockup = Ember.Application.create();
 
 /* Order and include as you please. */
+require('scripts/mixins');
 require('scripts/controllers/*');
 require('scripts/store');
 require('scripts/models/*');
